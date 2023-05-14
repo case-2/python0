7b2110b10b66fa8296786d883d1132e660faa69e
case-2 <case000002@gmail.com>
2023-05-15 04:39:45 +0530

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   counter.py

---
+++ counter.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+backpack = [
+  "sword", "rubber duck", "slice of pizza", "parachute",
+  "sword", "rubber duck", "slice of pizza", "parachute",
+  "sword", "rubber duck", "slice of pizza", "parachute",
+  "sword", "rubber duck", "slice of pizza", "parachute",
+  "cannon", "laser cannon", "canon 90D", "can of soup"
+]
+
+print(Counter(backpack))
